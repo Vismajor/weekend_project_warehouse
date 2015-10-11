4dbe59b9882a7780b5ccf74e6d52b3a555845fe7
Vismajor <podobaszalaizsolt@gmail.com>
2015-10-11 14:37:39 +0100

    project part 2

--- test.rb
+++ test.rb
@@ -1,4 +1,6 @@
-def array_of_bays 
+
+
+def array_of_items
   ['a10', 'a9', 'a8', 'a7', 'a6', 'a5', 'a4', 'a3', 'a2', 'a1', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'c10', 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'b9', 'b10']
 end
 
@@ -6,29 +8,25 @@ def hash_of_bays
  { 0 => "rubber band", 1 => "glow stick",  2 => "model car", 3 => "bookmark", 4 => "shovel", 5 => "rubber duck", 6 => "hanger", 7 => "blouse", 8 => "stop sign", 9 => "needle", 10 => "rusty nail", 11 => "drill press", 12 => "chalk", 13 => "word search", 14 => "thermometer", 15 => "face wash", 16 => "paint brush", 17 => "candy wrapper", 18 => "shoe lace", 19 => "leg warmers", 20 => "tire swing", 21 => "sharpie", 22 => "picture frame", 23 => "photo album", 24 => "nail filer", 25 => "tooth paste", 26 => "bath fizzers", 27 => "tissue box", 28 => "deodorant", 29 => "cookie jar"}
 end
 
-def get_bay
-  puts "Please type in which bay you need e.g a1 a2 a3"
-  bays = gets.chomp
+def get_items
+  puts "Please type in which items you need e.g leg warmers,tire swing,cookie jar"
+  items = gets.chomp
 
-  ind_bays = bays.split(" ")
+  picked_array_of_items = items.split(",")
 
- array_of_indexes = ind_bays.map do |bay|
-    array_of_bays.index(bay)
-  end
-sorted_array_of_indexes = array_of_indexes.sort
-distance_traveled = sorted_array_of_indexes.last - sorted_array_of_indexes.first
+#get the indexes of the items
+#then sort the indexes based on the new array
+#then then print out the bays based on a new hash.
+ #array_of_indexes = ind_bays.map do |bay|
+ #   array_of_bays.index(bay)
+ # end
+#sorted_array_of_indexes = array_of_indexes.sort
+#distance_traveled = sorted_array_of_indexes.last - sorted_array_of_indexes.first
 
-array_of_chosen_items = hash_of_bays.values_at(*array_of_bays).compact
-print array_of_chosen_items
-puts "The distance between the requested bays is #{distance_traveled}"
-puts "The items you requested are #{array_of_chosen_items}"
+#array_of_chosen_items = hash_of_bays.values_at(*sorted_array_of_indexes).compact
+
+#puts "The distance between the requested bays is #{distance_traveled}"
+#puts "The items you requested are #{array_of_chosen_items}"
 end
 
-
-get_bay
-
-
-
-
-
-
+get_items
--- warehouse.rb
+++ warehouse.rb
@@ -2,91 +2,29 @@ def array_of_bays
   ['a10', 'a9', 'a8', 'a7', 'a6', 'a5', 'a4', 'a3', 'a2', 'a1', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'c10', 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'b9', 'b10']
 end
 
-hash_of_bays = {
-  :a10 => "rubber band", 
-  :a9 => "glow stick", 
-  :a8 => "model car",
-  :a7 => "bookmark",
-  :a6 => "shovel",
-  :a5 => "rubber duck",
-  :a4 => "hanger",
-  :a3 => "blouse",
-  :a2 => "stop sign",
-  :a1 => "needle",
-  :c1 => "rusty nail",
-  :c2 => "drill press",
-  :c3 => "chalk",
-  :c4 => "word search",
-  :c5 => "thermometer",
-  :c6 => "face wash",
-  :c7 => "paint brush",
-  :c8 => "candy wrapper",
-  :c9 => "shoe lace",
-  :c10 => "leg warmers",
-  :b1 => "tire swing",
-  :b2 => "sharpie",
-  :b3 => "picture frame",
-  :b4 => "photo album",
-  :b5 => "nail filer",
-  :b6 => "tooth paste",
-  :b7 => "bath fizzers",
-  :b8 => "tissue box",
-  :b9 => "deodorant",
-  :b10 => "cookie jar",
-}
-
-#Enter string
-#Turn it into an array
-#
+def hash_of_bays
+ { 0 => "rubber band", 1 => "glow stick",  2 => "model car", 3 => "bookmark", 4 => "shovel", 5 => "rubber duck", 6 => "hanger", 7 => "blouse", 8 => "stop sign", 9 => "needle", 10 => "rusty nail", 11 => "drill press", 12 => "chalk", 13 => "word search", 14 => "thermometer", 15 => "face wash", 16 => "paint brush", 17 => "candy wrapper", 18 => "shoe lace", 19 => "leg warmers", 20 => "tire swing", 21 => "sharpie", 22 => "picture frame", 23 => "photo album", 24 => "nail filer", 25 => "tooth paste", 26 => "bath fizzers", 27 => "tissue box", 28 => "deodorant", 29 => "cookie jar"}
+end
 
 def get_bay
-  puts "Please type in which bay you need e.g a1,a2,a3"
+  puts "Please type in which bay you need e.g a1 a2 a3"
   bays = gets.chomp
 
-  # ['a1', 'b4', 'c3']
-  ind_bays = bays.split(",")
-  # [10, 4, 5]
-  
+  ind_bays = bays.split(" ")
+
  array_of_indexes = ind_bays.map do |bay|
     array_of_bays.index(bay)
   end
 sorted_array_of_indexes = array_of_indexes.sort
 distance_traveled = sorted_array_of_indexes.last - sorted_array_of_indexes.first
-symbols_of_chosen_bays = ind_bays.map { |symbols| symbols.to_sym }
 
-print "The distance between the requested bays is #{distance_traveled}"
-print "The items you requested are"
+array_of_chosen_items = hash_of_bays.values_at(*sorted_array_of_indexes).compact
+
+puts "The distance between the requested bays is #{distance_traveled}"
+puts "The items you requested are #{array_of_chosen_items}"
 end
 
-
-
-
-
-
-
-def get_distance
- 
-end
-
-def get_product_name
-
-end
-
-
-
-
-
-
-
-
-
-def journey_length(tram_line, start_station, end_station)
-  start_station_index = lothian_trams[tram_line].index(start_station)
-  end_station_index = lothian_trams[tram_line].index(end_station)
-  (start_station_index - end_station_index).abs
-end
-
-print get_bay
+get_bay
 
 
 
